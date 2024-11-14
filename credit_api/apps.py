d6f748244c109e7870cd2bb58a03b441ca1bbecd
Ritik Baba <ritik@floworks.ai>
2024-11-14 15:52:58 +0530

    apps and models added

---
+++ credit_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CreditApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'credit_api'
---
+++ credit_api/models.py
@@ -0,0 +1,29 @@
+# models.py
+from django.db import models
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    age = models.IntegerField(default=40)
+    monthly_salary = models.FloatField()
+    approved_limit = models.FloatField()
+    current_debt = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Loan(models.Model):
+    loan_id = models.AutoField(primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    loan_amount = models.FloatField()
+    tenure = models.IntegerField()
+    interest_rate = models.FloatField()
+    monthly_repayment = models.FloatField()
+    emis_paid_on_time = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"Loan {self.loan_id} for {self.customer}"
